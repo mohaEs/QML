7ced8aa2367b962182afff79c1c8ce9d909789c8
Gajan Mohanraj <gajanmohanraj@gmail.com>
2025-02-24 23:01:33 -0500

    Create Hybrid_Quantum-Classical_Resnet18.py

---
+++ models/Hybrid_Quantum-Classical_Resnet18.py
@@ -0,0 +1,112 @@
+import pennylane as qml
+from pennylane import numpy as np
+from pennylane.templates import RandomLayers
+import torch
+import torch.nn as nn
+import torchvision.models as models
+import torchvision.transforms as transforms
+from torch.utils.data import Dataset, DataLoader, Sampler
+import torch.optim as optim
+from torch.optim import lr_scheduler
+import torchvision
+from torchvision import datasets, transforms
+
+
+n_qubits = 4                # Number of qubits
+step = 0.0004               # Learning rate
+batch_size = 4              # Number of samples for each training step
+q_delta = 0.01              # Initial spread of random quantum weights
+start_time = time.time()    # Start of the computation timer
+
+def H_layer(nqubits):
+    """Layer of single-qubit Hadamard gates.
+    """
+    for idx in range(nqubits):
+        qml.Hadamard(wires=idx)
+
+
+def RY_layer(w):
+    """Layer of parametrized qubit rotations around the y axis.
+    """
+    for idx, element in enumerate(w):
+        qml.RY(element, wires=idx)
+
+
+def entangling_layer(nqubits):
+    """Layer of CNOTs followed by another shifted layer of CNOT.
+    """
+    for i in range(0, nqubits - 1, 2):
+        qml.CNOT(wires=[i, i + 1])
+    for i in range(1, nqubits - 1, 2):
+        qml.CNOT(wires=[i, i + 1])
+
+dev = qml.device("default.qubit", wires=n_qubits)
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+@qml.qnode(dev)
+def quantum_net(q_input_features, q_weights_flat):
+    """
+    The variational quantum circuit.
+    """
+
+    # Reshape weights
+    q_weights = q_weights_flat.reshape(q_depth, n_qubits)
+
+    # Start from state |+> , unbiased w.r.t. |0> and |1>
+    H_layer(n_qubits)
+
+    # Embed features in the quantum node
+    RY_layer(q_input_features)
+
+    # Sequence of trainable variational layers
+    for k in range(q_depth):
+        entangling_layer(n_qubits)
+        RY_layer(q_weights[k])
+
+    # Expectation values in the Z basis
+    exp_vals = [qml.expval(qml.PauliZ(position)) for position in range(n_qubits)]
+    return tuple(exp_vals)
+
+
+class DressedQuantumNet(nn.Module):
+
+    def __init__(self, q_depth, num_classes):
+
+        super().__init__()
+        self.pre_net = nn.Linear(512, n_qubits)
+        self.q_params = nn.Parameter(q_delta * torch.randn(q_depth * n_qubits))
+        self.post_net = nn.Linear(n_qubits, num_classes)
+
+    def forward(self, input_features):
+
+
+        pre_out = self.pre_net(input_features)
+        q_in = torch.tanh(pre_out) * np.pi / 2.0
+
+        # Apply the quantum circuit to each element of the batch and append to q_out
+        q_out = torch.Tensor(0, n_qubits)
+        q_out = q_out.to(device)
+        for elem in q_in:
+            q_out_elem = torch.hstack(quantum_net(elem, self.q_params)).float().unsqueeze(0)
+            q_out = torch.cat((q_out, q_out_elem))
+
+        # return the two-dimensional prediction from the postprocessing layer
+        return self.post_net(q_out)
+
+class QuantumHybridModel(nn.Module):
+  def __init__(self, q_depth, freeze, num_classes, device='cuda'):
+        super(QuantumHybridModel, self).__init__()
+
+        weights = torchvision.models.ResNet18_Weights.IMAGENET1K_V1
+        self.model = torchvision.models.resnet18(weights=weights)
+
+        if freeze:
+          for param in self.model.parameters():
+              param.requires_grad = False
+
+        self.model.fc = DressedQuantumNet(q_depth, num_classes)
+
+        self.model = self.model.to(device)
+
+  def forward(self, x):
+        return self.model(x)
