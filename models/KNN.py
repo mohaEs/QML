8eef37ec71ebbc481cbe8616cca18dc87591c3e6
john c. calhoon <77175104+greasyEggroll@users.noreply.github.com>
2025-04-19 14:26:01 -0400

    Update KNN.py

--- models/KNN.py
+++ models/KNN.py
@@ -2,22 +2,24 @@ from sklearn.neighbors import KNeighborsClassifier
 from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
 import os
 
-os.environ["OMP_NUM_THREADS"] = "1"
-
 def run_knn(X_train, y_train, X_val, y_val, X_test, y_test):
     knn = KNeighborsClassifier(n_neighbors=5, weights='uniform')
     knn.fit(X_train, y_train)
+    
     val_pred = knn.predict(X_val)
     val_accuracy = accuracy_score(y_val, val_pred)
     val_precision = precision_score(y_val, val_pred, average='weighted')
     val_recall = recall_score(y_val, val_pred, average='weighted')
     val_f1 = f1_score(y_val, val_pred, average='weighted')
+    
     y_pred = knn.predict(X_test)
     y_prob = knn.predict_proba(X_test)
+    
     accuracy = accuracy_score(y_test, y_pred)
     precision = precision_score(y_test, y_pred, average='weighted')
     recall = recall_score(y_test, y_pred, average='weighted')
     f1 = f1_score(y_test, y_pred, average='weighted')
+    
     return {
         'y_pred': y_pred,
         'y_prob': y_prob,
