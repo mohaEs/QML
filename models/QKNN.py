1ef4702504dc326b46d9651856e975f24dd2bf54
john c. calhoon <77175104+greasyEggroll@users.noreply.github.com>
2025-04-19 14:27:00 -0400

    Update QKNN.py

--- models/QKNN.py
+++ models/QKNN.py
@@ -1,59 +1,8 @@
 import pennylane as qml
 from pennylane import numpy as np
-from pennylane.templates import RandomLayers
-import tensorflow as tf
-from tensorflow import keras
-import matplotlib.pyplot as plt
-import os
-from google.colab import drive
-import cv2
-from sklearn.model_selection import train_test_split
-import pandas as pd
-import torch
-import torch.nn as nn
-import torchvision.models as models
-import torchvision.transforms as transforms
-from torch.utils.data import Dataset, DataLoader, Sampler
-import torch.optim as optim
-from torch.optim import lr_scheduler
-import torchvision
-from torchvision import datasets, transforms
-from PIL import Image
-import time
-import os
-import copy
-from torch.optim.lr_scheduler import CosineAnnealingLR
-import seaborn as sns
-from sklearn.metrics import (
-    accuracy_score, f1_score, roc_auc_score, confusion_matrix, roc_curve,
-    precision_recall_curve, auc
-)
-from sklearn.preprocessing import label_binarize
-import random
-from sklearn.preprocessing import MaxAbsScaler
-from sklearn.preprocessing import StandardScaler
-import pickle
-from sklearn.preprocessing import LabelEncoder
-from sklearn.decomposition import PCA
-from sklearn.svm import SVC
-from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
-import matplotlib.pyplot as plt
-import seaborn as sns
-from collections import Counter
-from sklearn.preprocessing import LabelBinarizer
 from sklearn.neighbors import KNeighborsClassifier
-from sklearn.utils.class_weight import compute_class_weight
-import numpy as np
-import pickle
-import time
-from sklearn.preprocessing import LabelEncoder, StandardScaler
-from sklearn.decomposition import PCA
-from sklearn.utils import shuffle
-from sklearn.svm import SVC
-from sklearn.metrics import accuracy_score, roc_curve, auc, precision_score, recall_score, f1_score
-
-os.environ["OMP_NUM_THREADS"] = "1"
-
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
+import os
 
 num_qubits = 4
 batch_size = 4
